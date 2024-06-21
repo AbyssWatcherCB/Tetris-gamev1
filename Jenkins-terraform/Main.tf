abaff72e74f893abd532cfa45de1e3d928af7c02
AbyssWatcherCB <128981731+AbyssWatcherCB@users.noreply.github.com>
2024-06-21 22:04:07 +0100

    update

--- Jenkins-terraform/Main.tf
+++ Jenkins-terraform/Main.tf
@@ -1,23 +1,18 @@
-provider "aws" {
-  region = "us-east-1" # Set to your preferred region
-}
 
 resource "aws_iam_role" "example_role" {
   name = "Jenkins-terraform"
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "ec2.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-}
-EOF
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
 }
 
 resource "aws_iam_role_policy_attachment" "example_attachment" {
@@ -32,19 +27,36 @@ resource "aws_iam_instance_profile" "example_profile" {
 
 resource "aws_security_group" "Jenkins-sg" {
   name        = "Jenkins-Security Group"
-  description = "Open 22,443,80,8080,9000,3000"
+  description = "Open ports for Jenkins"
 
   ingress = [
-    for port in [22, 80, 443, 8080, 9000, 3000] : {
-      description      = "Allow TCP traffic"
-      from_port        = port
-      to_port          = port
+    {
+      description      = "SSH from VPC"
+      from_port        = 22
+      to_port          = 22
       protocol         = "tcp"
       cidr_blocks      = ["0.0.0.0/0"]
-      ipv6_cidr_blocks = []
-      prefix_list_ids  = []
-      security_groups  = []
-      self             = false
+    },
+    {
+      description      = "HTTP from VPC"
+      from_port        = 80
+      to_port          = 80
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+    },
+    {
+      description      = "HTTPS from VPC"
+      from_port        = 443
+      to_port          = 443
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+    },
+    {
+      description      = "Jenkins HTTP"
+      from_port        = 8080
+      to_port          = 8080
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
     }
   ]
 
@@ -60,34 +72,16 @@ resource "aws_security_group" "Jenkins-sg" {
   }
 }
 
-resource "aws_vpc" "example_vpc" {
-  cidr_block = "10.0.0.0/16"
-
-  tags = {
-    Name = "example-vpc"
-  }
-}
-
-resource "aws_subnet" "example_subnet" {
-  vpc_id     = aws_vpc.example_vpc.id
-  cidr_block = "10.0.1.0/24"
-
-  tags = {
-    Name = "example-subnet"
-  }
-}
-
 resource "aws_instance" "web" {
-  ami                    = "ami-0c55b159cbfafe1f0" # Example AMI ID for us-east-1
-  instance_type          = "t2.large"
-  key_name               = "Argo key"
-  subnet_id              = aws_subnet.example_subnet.id
+  ami                    = "ami-055744c75048d8296"
+  instance_type          = "t2.medium"
+  key_name               = "testgame"
   vpc_security_group_ids = [aws_security_group.Jenkins-sg.id]
   user_data              = templatefile("./install_jenkins.sh", {})
   iam_instance_profile   = aws_iam_instance_profile.example_profile.name
 
   tags = {
-    Name = "Jenkins Server"
+    Name = "Jenkins-argo"
   }
 
   root_block_device {
